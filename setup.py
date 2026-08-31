8e4584f9c32a705a876f137cde3cb795e16e87d2
everoddandeven <everoddandeven@protonmail.com>
2026-08-31 19:42:05 +0200

    bump version to v0.9.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from skbuild import setup
 
 setup(
     name='monero',
-    version='0.9.3',
+    version='0.9.4',
     author='everoddandeven',
     author_email="everoddandeven@protonmail.com",
     maintainer='everoddandeven',
