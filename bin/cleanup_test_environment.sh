26c7ea89616e2c82c76ee111b233881a55f9854f
everoddandeven <everoddandeven@protonmail.com>
2026-09-13 20:54:08 +0200

    tests: improve clean up script

--- bin/cleanup_test_environment.sh
+++ bin/cleanup_test_environment.sh
@@ -2,5 +2,9 @@
 
 # remove docker containers
 sudo docker compose -f tests/docker-compose.yml down -v
-rm -rf test_wallets
-rm monero_tests_*
+rm -rf test_wallets 2>&1
+rm monero_tests_* 2>&1
+rm -rf .pytest_cache 2>&1
+rm -rf __pycache__ 2>&1
+rm -rf tests/__pycache__ 2>&1
+rm -rf tests/utils/__pycache__ 2>&1
